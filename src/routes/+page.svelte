2a67746635c2dfdae94b10820a9022076333e5fb
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-10-13 17:09:00 +0200

    Init sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+    <title>Image completion prototype</title>
+</svelte:head>
+
+<script>
+
+</script>
+
+<video id="videoElement" autoplay></video>
+<canvas id="canvasElement"></canvas>
+
+<style>
+  video {
+  transform: scaleX(-1);
+  }
+</style>

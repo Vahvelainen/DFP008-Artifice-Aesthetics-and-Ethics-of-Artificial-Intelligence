2133a78edd0cfc5f2ff0582366a1ce0468415b1e
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-11-24 11:45:34 +0100

    Adapt image generation to new design

--- src/apps/imageCompletion/ImageSide.svelte
+++ src/apps/imageCompletion/ImageSide.svelte
@@ -1,30 +1,82 @@
 <script>
+
+  import generateImage from "./scripts/imageGeneration"
+
   import Input from "./Input.svelte"
   import Output from './Output.svelte'
   import WideButton from "@lib/WideButton.svelte"
 
-  import imageStore from './scripts/imageStore' 
+  import imageStore, {saveOutputUrl} from './scripts/imageStore' 
 
-  //we use this to toggle between showing result etc
-
-  //Image generation is propably 
-
-  let inputting = true
+  let inputting = true //true wehn inputting new image
   let canvas
+  let inputBlob
+
+  export let promt
+
+  //Dynamic function that changes when promt changes
+  $: {
+    handlePromtUpdate(promt)
+  }
+
+  function handlePromtUpdate(promt) {
+    console.log('Current promt: ', promt)
+    if ( !inputting ) {
+      startGenerate()
+    }
+  } 
+
+  async function startGenerate() {
+    console.log('Start image generation');
+
+    if (inputting) {
+      inputBlob = await captureFrame(canvas)
+      //Temporalily set the input image as output
+      saveOutputUrl(URL.createObjectURL(inputBlob));
+    }    
+    inputting = false
+
+    //Upload input to firebase and set it to a store
+    console.log('Input blob:', inputBlob)
+    generateImage(
+      //Promt
+      promt,
+      //Negative promt
+      'people, clutter, background (drawing) (painting)',
+      inputBlob, 'NEW IXD TEST') //Input description is saved separately to database
+  }
+
+  /** @param {HTMLCanvasElement} canvas */
+  function captureFrame(canvas) {
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (blob) {
+          // Resolve the promise with the blob so it can be used outside this function
+          resolve(blob);
+        } else {
+          // Reject the promise if no blob was created
+          reject(new Error('Could not retrieve blob from canvas'));
+        }
+      }, 'image/jpeg');
+    });
+  }
+
 </script>
 
 <div class="parent">
   {#if inputting}
     <Input bind:canvas/>
-    <WideButton on:click={ () => inputting = false } >Check The Outcome</WideButton>
-  {:else if $imageStore.loading}
-    <div class="loading-spinner"></div>
+    <WideButton on:click={startGenerate} >Check The Outcome</WideButton>
   {:else}
     <Output/>
-    <div class="two-buttons">
-      <WideButton on:click={ () => inputting = true } >Retry</WideButton>
-      <WideButton>I am happy</WideButton>
-    </div>
+    {#if $imageStore.loading}
+      <div class="loading-spinner"></div>
+    {:else}
+      <div class="two-buttons">
+        <WideButton on:click={ () => inputting = true } >Retry</WideButton>
+        <WideButton>I am happy</WideButton>
+      </div>
+    {/if}
   {/if}
 </div>
 
@@ -40,14 +92,16 @@
     grid-gap: 1em;
   }
   .loading-spinner {
-    height: 6em;
-    width: 6em;
+    height: 4em;
+    width: 4em;
     animation: rotateAnimation 2s infinite linear;
 
-    border-width: 0.5em;
+    border-width: 0.35em;
     border-style: solid;
     border-color: white white transparent white;
     border-radius: 50%;
+
+    margin: auto;
   }
   @keyframes rotateAnimation {
     from {
--- src/apps/imageCompletion/Main.svelte
+++ src/apps/imageCompletion/Main.svelte
@@ -4,37 +4,17 @@
    * Error handling
   */
 
-  import generateImage from "./scripts/imageGeneration"
-
   import ImageSide from "./ImageSide.svelte";
   import PromtMenu from "./PromtMenu.svelte";
   
-  let inputCanvas //Binded to the export from input
-  let inputDescription = 'Mixer that is fun to use'
-
-  /** @param {HTMLCanvasElement} canvas */
-  function captureFrame(canvas) {
-    canvas.toBlob(blob => {
-      console.log(blob);
-      //Upload input to firebase and set it to a store
-      generateImage(
-        //Promt
-        'Turn the object in the picture into a sleek product design of ' +
-        inputDescription +
-        ', in the style of Stefano Marzano, philips design' +
-        ', realism, rendering, futurism, --q2',
-        //Negative promt
-        'people, clutter, background (drawing) (painting)',
-        blob, inputDescription) //Input description is saved separately to database
-    }, 'image/jpeg');
-  }
+  let promt //bonded to promt menu, passed to image side
 
 </script>
 
 <section class="image-generation">
   <!-- These might be should be stylised here for the widths to make them work -->
-  <PromtMenu/>
-  <ImageSide/>
+  <PromtMenu bind:promt />
+  <ImageSide promt={promt}/>
 </section>
 
 <style>

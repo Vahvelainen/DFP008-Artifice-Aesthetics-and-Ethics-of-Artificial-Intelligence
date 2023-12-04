dfb2a8f1a45e40df8d85ebf064589674c4174451
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-12-04 19:10:14 +0100

    Refactor image generation to main

--- src/apps/imageCompletion/ImageSide.svelte
+++ src/apps/imageCompletion/ImageSide.svelte
@@ -1,72 +1,22 @@
 <script>
-
-  import generateImage from "./scripts/imageGeneration"
-
   import Input from "./Input.svelte"
   import Frame from '@lib/Frame.svelte'
   import WideButton from "@lib/WideButton.svelte"
 
-  import imageStore, {saveOutputUrl} from './scripts/imageStore' 
+  let dispatch = createEventDispatcher()
 
-  let inputting = true //true wehn inputting new image
-  let canvas
-  let inputBlob
+  import imageStore from './scripts/imageStore' 
+  import { createEventDispatcher } from "svelte";
 
-  export let promt
-
-  //Dynamic function that changes when promt changes
-  $: {
-    handlePromtUpdate(promt)
-  }
-
-  function handlePromtUpdate(promt) {
-    console.log('Current promt: ', promt)
-    if ( !inputting ) {
-      startGenerate()
-    }
-  } 
-
-  async function startGenerate() {
-    console.log('Start image generation');
-
-    if (inputting) {
-      inputBlob = await captureFrame(canvas)
-      //Temporalily set the input image as output
-      saveOutputUrl(URL.createObjectURL(inputBlob));
-    }    
-    inputting = false
-
-    //Upload input to firebase and set it to a store
-    console.log('Input blob:', inputBlob)
-    generateImage(
-      //Promt
-      promt,
-      //Negative promt
-      'people',
-      inputBlob, 'NEW IXD TEST') //Input description is saved separately to database
-  }
-
-  /** @param {HTMLCanvasElement} canvas */
-  function captureFrame(canvas) {
-    return new Promise((resolve, reject) => {
-      canvas.toBlob(blob => {
-        if (blob) {
-          // Resolve the promise with the blob so it can be used outside this function
-          resolve(blob);
-        } else {
-          // Reject the promise if no blob was created
-          reject(new Error('Could not retrieve blob from canvas'));
-        }
-      }, 'image/jpeg');
-    });
-  }
+  export let inputting //true wehn inputting new image
+  export let canvas
 
 </script>
 
 <div class="parent">
   {#if inputting}
     <Input bind:canvas/>
-    <WideButton on:click={startGenerate} >Check The Outcome</WideButton>
+    <WideButton on:click={ () => dispatch('generate') } >Check The Outcome</WideButton>
   {:else}
     <!-- Image output -->
     <Frame>
--- src/apps/imageCompletion/Main.svelte
+++ src/apps/imageCompletion/Main.svelte
@@ -3,18 +3,74 @@
    * TODO:
    * Error handling
   */
-
+ 
   import ImageSide from "./ImageSide.svelte";
   import PromtMenu from "./PromtMenu.svelte";
-  
+ 
+  import generateImage from "./scripts/imageGeneration"
+  import {saveOutputUrl} from './scripts/imageStore' 
+
   let promt //bonded to promt menu, passed to image side
+  let canvas //binded to image side for input
+  let inputting = true //also binded to imageSide
+  let inputBlob
+
+  //Dynamic function that changes when promt changes
+  $: {
+    handlePromtUpdate(promt)
+  }
+
+  function handlePromtUpdate(promt) {
+    console.log('Current promt: ', promt)
+    if ( !inputting ) {
+      startGenerate()
+    }
+  } 
+
+  async function startGenerate() {
+    console.log('Start image generation');
+
+    if (inputting) {
+      inputBlob = await captureFrame(canvas)
+      //Temporalily set the input image as output
+      saveOutputUrl(URL.createObjectURL(inputBlob));
+    }    
+    inputting = false
+
+    console.log('Input blob:', inputBlob)
+    generateImage(
+      //Promt
+      promt,
+      //Negative promt
+      'people',
+      inputBlob, 'NEW IXD TEST') //Input description is saved separately to database
+  }
+
+  /** @param {HTMLCanvasElement} canvas */
+  function captureFrame(canvas) {
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (blob) {
+          // Resolve the promise with the blob so it can be used outside this function
+          resolve(blob);
+        } else {
+          // Reject the promise if no blob was created
+          reject(new Error('Could not retrieve blob from canvas'));
+        }
+      }, 'image/jpeg');
+    });
+  }
 
 </script>
 
 <section class="image-generation">
   <!-- These might be should be stylised here for the widths to make them work -->
   <PromtMenu bind:promt />
-  <ImageSide promt={promt}/>
+  <ImageSide
+    bind:canvas
+    bind:inputting
+    on:generate={startGenerate} 
+  />
 </section>
 
 <style>

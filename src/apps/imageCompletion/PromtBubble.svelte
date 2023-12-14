0629cf2cfa4c83d32297b2b03bbf377ef46751b6
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-12-14 23:09:30 +0100

    Add overlay to promtbubbles

--- src/apps/imageCompletion/PromtBubble.svelte
+++ src/apps/imageCompletion/PromtBubble.svelte
@@ -4,25 +4,25 @@
   export let selection // with properties of name and img url (and promts but not used here)
   export let angle //This is goign to be a fun one
 
-  let backGroundImage = selection ? selection.img : '' 
-
   //How far away should the bubble be
   const radiusPercentage = .70
-
+  
   //Calculate positions of the bubble
   const centerX = 50; // Center X coordinate of the clock face as percentage
   const centerY = 50; // Center Y coordinate of the clock face as percentage
   const radius = radiusPercentage * 50; // Radius of the arm as percentage of clock face size
-
+  
   const radian = angle * (Math.PI / 180); // Convert angle to radian
   const top = centerY - (Math.sin(radian) * radius);
   const left = centerX + (Math.cos(radian) * radius);
-
+  
+  $: backGroundImage = selection ? selection.img : '' 
   $: dynamicStylStr = `top:${top}%;left:${left}%;background-image:url('${backGroundImage}')`
 
 </script>
 
 <button class="bubble" on:click style={dynamicStylStr}>
+  <div class="overlay"></div>
   <h3>{name}</h3>
   {#if selection}
     <span>{selection.name}</span>
@@ -48,5 +48,15 @@
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+    overflow: hidden;
+  }
+
+  div.overlay {
+    position: absolute;
+    background-color: white;
+    opacity: 0.5;
+    width: 100%;
+    height: 100%;
+    z-index: -50;
   }
 </style>

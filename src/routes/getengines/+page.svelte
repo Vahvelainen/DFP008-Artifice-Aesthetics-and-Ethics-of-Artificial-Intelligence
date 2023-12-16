158128b08078dbd4d9fc55fa87b1e45468c8e27c
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-12-16 12:05:12 +0100

    Move getEngines to apps

--- src/routes/getengines/+page.svelte
+++ src/routes/getengines/+page.svelte
@@ -1,38 +1,9 @@
+<svelte:head>
+    <title>Stability.ai engines list</title>
+</svelte:head>
+
 <script>
-  //Ugly duckling for seeing engines from stabilityAI
-
-  import { functions } from '@src/firebase'
-  import { httpsCallable } from "firebase/functions"
-  import { onMount } from 'svelte'
-
-  let engines = []
-
-  const getEnginesCallable = httpsCallable(functions, 'getEngines');
-
-  async function getEngines() {
-    const response = await getEnginesCallable();
-    const data = response.data
-    console.log(data)
-    engines = data
-    return response.data
-  }
-
-  onMount( () => {
-    getEngines()
-  })
-
+  import GetEngines from "@apps/getEngines.svelte";
 </script>
 
-
-<h1>StabilityAI available engines:</h1>
-
-<ul>
-  {#each engines as engine }
-    <li>
-      <h2>{engine.name}</h2>
-      <p>{engine.description}</p>
-      <p>{engine.id}</p>
-      <p>{engine.type}</p>
-    </li>    
-  {/each}
-</ul>
+<GetEngines/>

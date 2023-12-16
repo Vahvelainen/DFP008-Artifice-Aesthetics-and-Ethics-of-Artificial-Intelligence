757e6f26435a0f07ef7bc1c0015cd0c6be0fb57c
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-12-16 12:05:35 +0100

    Move history to Apps

---
+++ src/apps/history.svelte
@@ -0,0 +1,48 @@
+
+<script>
+
+  import { db } from "@src/firebase"
+  import { collection, getDocs, query, orderBy, limit, onSnapshot } from "firebase/firestore"; 
+  import { onDestroy, onMount } from "svelte";
+  
+  let generations = []
+  let unsubscribe = ()=>{}
+  
+  async function getHistory() {
+    const ref = collection(db, "generations_v2")
+    const q = query(ref, orderBy("created", "desc"), limit(50));
+    
+    unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let generations_temp = []
+      querySnapshot.forEach((doc) => {
+        generations_temp.push({
+          id: doc.id,
+          ...doc.data()
+        })
+      });
+      console.log(generations_temp)
+      generations = generations_temp
+    });
+  }
+  
+  onMount(getHistory) 
+  onDestroy(unsubscribe)
+  
+  </script>
+  
+  <section>
+    {#each generations as generation (generation.id) }
+      <img src="{generation.output}" alt="previous cretion of somethng">
+    {/each}
+  </section>
+  
+  <style>
+    section {
+      display: flex;
+      flex-wrap: wrap;
+    }
+    img {
+      max-width: 25%;
+    }
+  </style>
+  

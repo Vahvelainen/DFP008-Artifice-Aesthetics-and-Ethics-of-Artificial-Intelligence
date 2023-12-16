b1534fad2696cc8b2b04cc8e4d1db984cde66eaa
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-12-16 12:00:37 +0100

    Remove legacy version

--- src/lib/Frame.svelte
+++ src/lib/Frame.svelte
@@ -1,10 +1,8 @@
 <script>
-  export let subframe = false
-  export let small = false
   export let unbound = false
 </script>
 
-<div class="frame{ subframe ? ' subframe' : '' }{ small ? ' small' : '' }{ unbound ? ' unbound' : '' }">
+<div class="frame{ unbound ? ' unbound' : '' }">
   <slot></slot>
 </div>
 
@@ -16,25 +14,12 @@
     justify-content: center;
     align-items: center;
 
-    /* max-width: 640px; */
     background-color: #000000;
     width: 100%;
     aspect-ratio: 640/512;
     border-radius: 10px;
-    /* border: 2px solid #00DEC6; */
     overflow: hidden;
     margin-bottom: 1em;
-  }
-  .subframe{
-    position: absolute;
-    left: 1em;
-    bottom: 1em;
-    margin: 0;
-  }
-  .small{
-    width: 8em;
-    border-radius: 6px;
-    border: 1px solid #00DEC6;
   }
   .unbound{
     border: none;
--- src/routes/old/+page.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-    <title>Image completion prototype</title>
-</svelte:head>
-
-<script>
-  import Main from "@apps/imageCompletionOld/Main.svelte";
-</script>
-
-<Main/>
